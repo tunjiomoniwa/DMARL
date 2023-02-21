5a89375267c83da5213eecfc3ec996c3b005ca74
tunjiomoniwa <46023480+tunjiomoniwa@users.noreply.github.com>
2023-02-21 00:13:56 +0000

    action_selection

---
+++ DQLSI_Q_learning/action_selection.m
@@ -0,0 +1,7 @@
+function action = action_selection(obs, obs_over, MAX_user_connections, epsilon, current_state, Q)
+
+    if obs ||obs_over>=MAX_user_connections
+        action = 7;
+    else
+        action = select_action1(epsilon, current_state, Q);          
+end
