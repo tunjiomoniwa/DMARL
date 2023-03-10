431a742e2cd2e07cab3a8d68c00c33e9357a5b1f
tunjiomoniwa <46023480+tunjiomoniwa@users.noreply.github.com>
2023-03-10 00:11:39 +0000

    Update sumo_position_data.py

--- DAMAD and DACEMAD Collaborative Variants/sumo_position_data.py
+++ DAMAD and DACEMAD Collaborative Variants/sumo_position_data.py
@@ -50,4 +50,23 @@ def update_M50(kk):
         return x, y
 
 
-#print(update_high(600))
+def update_DCC_Ped(kk, file): 
+        
+        x= []
+        y= []
+        #m50_saturation_done
+        #m50_congested_done.csv
+        #N7_saturation
+        #N7_freeflow
+        #DCC_freeflow_A
+        #N7_congested
+        #with open("sumo_M50_freeflow.csv",newline='') as f:
+        with open(file,newline='') as f:
+                ereader = csv.DictReader(f)                
+                for row in ereader:
+                        #print(type(float(row['time'])))
+                        if float(row['time']) == kk:                                 
+                                x.append(float(row['xnew'])*1000)
+                                y.append(float(row['ynew'])*1000)                        
+        return x, y
+
